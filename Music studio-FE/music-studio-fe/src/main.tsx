b3578a965f71aa470e6ea54993154677cdc3ed6a
indumakaweeshvara <indumakawee4@gmail.com>
2026-01-06 12:45:53 +0530

    Switch to HashRouter for reliable GitHub Pages deployment - WORKING

--- Music studio-FE/music-studio-fe/src/main.tsx
+++ Music studio-FE/music-studio-fe/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './routes/redux/store';
 import AppRoutes from './routes/AppRoutes';
@@ -9,9 +9,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/musicstudiomanagment">
+            <HashRouter>
                 <AppRoutes />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
 );
